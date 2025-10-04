bf3a68b1a75234eeb50e376ae0052cf65ad477c1
Jessica Huang <151045619+Messyjessyyy@users.noreply.github.com>
2025-10-04 17:30:43 -0400

    Create 0128.py

---
+++ 0128.py
@@ -0,0 +1,30 @@
+#Longest Consective Number 
+
+#Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+
+# Your algorithm should run in O(n) complexity.
+# Example:
+# Input: [100, 4, 200, 1, 3, 2]
+# Output: 4
+
+class Solution:
+    def longestConsecutive(self, nums):
+        if not nums:
+            return 0
+        
+        num_set = set(nums)
+        longest_streak = 0
+        
+        for num in num_set:
+            if num - 1 not in num_set:  # only start counting if 'num' is the start of a sequence
+                current_num = num
+                current_streak = 1
+                
+                while current_num + 1 in num_set:
+                    current_num += 1
+                    current_streak += 1
+                
+                longest_streak = max(longest_streak, current_streak)
+        
+        return longest_streak
+
